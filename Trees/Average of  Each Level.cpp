375320df0bbd1f24704fa4d893da8a1ac28b5fb9
Pooja Rathore <poojarathore.nks@gmail.com>
2020-08-06 17:17:06 +0530

    Average of Levels in Tree

---
+++ Trees/Average of  Each Level.cpp
@@ -0,0 +1,27 @@
+vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> f;
+        queue<TreeNode*> q;
+        double res;
+        if(root==NULL) return f;
+        TreeNode *temp;
+        q.push(root);
+        while(1)
+        {
+            double sum=0;
+            int size=q.size();
+            int i=0;
+            if(q.empty()) break;
+            while(i<size)
+            {
+                temp=q.front();
+                q.pop();
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+                sum+=temp->val;
+                i++;
+            }
+            res=(double)sum/size;
+            f.push_back(res);
+        }
+        return f;
+    }
