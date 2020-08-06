7956dbcc2a71a2cdd19034f5cde542da45baf5f6
Pooja Rathore <poojarathore.nks@gmail.com>
2020-08-06 13:11:45 +0530

    Height of tree

---
+++ Trees/Height_of_tree.cpp
@@ -0,0 +1,20 @@
+//Usual driver code
+//struct definition
+struct Node{
+    int val;
+    Node *left,*right;
+    Node(int x)
+    {
+        val=x;
+        left=NULL;
+        right=NULL;
+    }
+};
+//function to find the height of tree
+int height(Node* root)
+{
+   // Your code here
+   if(root==NULL) return 0;
+   if(root->left==NULL && root->right==NULL) return 1;
+   return 1+height(root->left)>1+height(root->right)?1+height(root->left):1+height(root->right);
+}
