5073ed9175e03172a2f2ac2497bf0a12fdf4dac1
Pooja Rathore <poojarathore.nks@gmail.com>
2020-08-06 17:06:57 +0530

    Level Order Traversal

---
+++ Trees/Binary Tree level Order Traversal.cpp
@@ -0,0 +1,27 @@
+//logic appplied suing queue and finally reversing the obtained vector of vector
+vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<vector<int>> f;
+         if(root==NULL) return f;
+        TreeNode * temp;
+       
+        q.push(root);
+        while(1){
+            int size=q.size();
+            vector<int> v;
+            if(q.empty()) break;
+            int i=0;
+            while(i<size)
+            {
+               temp=q.front();
+                q.pop();
+                v.push_back(temp->val);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+                i++;
+            }
+            f.push_back(v);
+        }
+        reverse(f.begin(),f.end());
+        return f;
+    }
